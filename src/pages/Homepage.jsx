d2ca023f2b944f2e9a9bf7fbff1a9fc29ab89ce3
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2025-12-06 21:52:31 +0530

    asdfasdf

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,10 @@
+import HeroSection from "../components/Herosection";
 import { ProductSection } from "../components/ProductSection";
 
 const Homepage = () => {
   return (
     <div>
+      <HeroSection />
       <ProductSection />
     </div>
   );
